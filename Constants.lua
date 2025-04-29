b97176ea24231f440d76c74e10399dfbed8d3bab
telandis0008 <telandis3348@gmail.com>
2025-04-29 11:38:40 -0700

    Add files via upload

---
+++ Constants.lua
@@ -0,0 +1,71 @@
+--[[
+
+= DigitalSwirl Client =
+
+Source: ControlScript/Constants.lua
+Purpose: Common game constants
+Author(s): Regan "CuckyDev/TheGreenDeveloper" Green
+
+--]]
+
+return {
+	--Game framerate
+	framerate = 60,
+
+	--Player states
+	state = {
+		idle = 0,
+		walk = 1,
+		skid = 2,
+		spindash = 3,
+		roll = 4,
+		airborne = 5,
+		homing = 6,
+		bounce = 7,
+		rail = 8,
+		light_speed_dash = 9,
+		air_kick = 10,
+		ragdoll = 11,
+		hurt = 12,
+		dead = 13,
+		drown = 14,
+		inactive = 15,
+		peelout = 16,
+		airboost = 17,
+		dropdashcharging = false,
+		dropcharging = 18,
+		pulley = 19,
+		pole = 20,
+		skydive = 21,
+		trickwait = 22,
+		board = 23,
+		climb = 24,
+		walljump = 25,
+		rollal = 26,
+		goalring = 27,
+		mach = 28,
+		crouch = 29,
+		crawl = 30,
+		slide = 31,
+		kick = 32,
+		DoubleJump = 33,
+		freefall = 34,
+		spindash2 = 35,
+		stop = 36,
+		scripted = 37,
+		freestyletrick = 38,
+		boost = 39,
+		float = 40,
+		fan = 41,
+		wallrun = 42,
+		sidestep = 43,
+		jump_pad = 44,
+		float2 = 45,
+		super_dash = 46,
+		charge = 47,
+		goalring2 = 48,
+		spinkick = 49,
+		chaosblast = 50,
+		homingdash = 51,
+	},
+}
